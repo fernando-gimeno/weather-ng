f9bf4ed6dcac88bc5fd5bfee18577bac1f801f7b
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-06-21 19:31:53 -0300

    Delete src/environments directory

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  weatherApi: {
-    baseUrl: 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline',
-    apiKey: '2CDHDVPCCGWUBUULGK9ZQ8XRU',
-  },
-};
